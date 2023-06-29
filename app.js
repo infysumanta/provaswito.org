15cd4e31adbd9b2d6f3525d6c9169ce22b3c62d4
Sumanta Kabiraj <sumantablog@gmail.com>
2023-06-30 04:22:00 +0530

    update

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", function (_req, res) {
-    res.render("index");
+    res.render("index.ejs");
 });
 app.get("/workshop", function (_req, res) {
-    res.render("workshop");
+    res.render("workshop.ejs");
 });
 app.get("/art-exhibition", function (_req, res) {
-    res.render("art-exhibition");
+    res.render("art-exhibition.ejs");
 });
 app.get("/cultural-program", function (_req, res) {
-    res.render("cultural-program");
+    res.render("cultural-program.ejs");
 });
 app.get("/other-event", function (_req, res) {
-    res.render("other-event");
+    res.render("other-event.ejs");
 });
 app.get("/news-tv-cover", function (_req, res) {
-    res.render("news-tv-cover");
+    res.render("news-tv-cover.ejs");
 });
 app.get("/news-paper-cover", function (_req, res) {
-    res.render("news-paper-cover");
+    res.render("news-paper-cover.ejs");
 });
 
 module.exports = app;
