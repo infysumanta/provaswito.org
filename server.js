fef00e16c619064f2a7d3ec55664235f707c99af
Sumanta Kabiraj <sumantablog@gmail.com>
2023-06-30 02:09:41 +0530

    provaswito.org

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", function (_req, res) {
+  res.render("index");
+});
+app.get("/workshop", function (_req, res) {
+  res.render("workshop");
+});
+app.get("/art-exhibition", function (_req, res) {
+  res.render("art-exhibition");
+});
+app.get("/cultural-program", function (_req, res) {
+  res.render("cultural-program");
+});
+app.get("/other-event", function (_req, res) {
+  res.render("other-event");
+});
+app.get("/news-tv-cover", function (_req, res) {
+  res.render("news-tv-cover");
+});
+app.get("/news-paper-cover", function (_req, res) {
+  res.render("news-paper-cover");
+});
+
+app.listen(8001, () => {
+  console.log("Listening on Port 5000");
+});
